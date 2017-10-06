78805176772f6ee33cc8fa8db315983a65f341b1
DolanL <32385280+DolanL@users.noreply.github.com>
2017-10-06 11:26:39 +0300

    Update and rename LICENSE.md to main.cpp

---
+++ LICENSE.md/main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int n,m;
+cout<<"razmer 1 yacheiki"<<endl;
+cin>>x;
+cout<<"razmer 2 yacheiki"<<endl;
+cin>>y;
+int a[n][m];
+int b[n][m];
+cout<<"vvod elementov 1 massiva"<<endl;
+for(int i=0;i<n;i++)
+for(int j=0;j<m;j++)
+cin>>a[i][j];
+cout<<"vvod elementov 2 massiva"<<endl;
+for(int i=0;i<n;i++)
+for(int j=0;j<m;j++)
+cin>>b[i][j];
+
+for(int i=0;i<n;i++)
+{
+for(int j=0;j<m;j++)
+cout<<a[i][j]+b[i][j]<<" ";
+cout<< endl;
+}
+
+
+return 0;
+} 
+
