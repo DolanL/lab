4990684503d4ac77a9eba1d7b5bb024e1dd98808
DolanL <32385280+DolanL@users.noreply.github.com>
2017-10-15 13:41:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <iostream>
+#include <stdio.h>
 using namespace std;
 
 int main()
@@ -10,22 +11,28 @@ int main()
 	cin >> m;
 	int a[n][m];
 	int b[n][m];
+	int x[n][m];
 	cout << "vvod elementov 1 massiva" << endl;
-	for (int i = 0; i<n; i++)
-		for (int j = 0; j<m; j++)
+	for (int i = 0; i < n; i++){
+	for (int j = 0; j < m; j++){
 			cin >> a[i][j];
+	}
+	}
+		
 	cout << "vvod elementov 2 massiva" << endl;
-	for (int i = 0; i<n; i++)
-		for (int j = 0; j<m; j++)
+	for (int i = 0; i<n; i++){
+	for (int j = 0; j<m; j++){
 			cin >> b[i][j];
+	}
+	};
+	cout<<endl;
 
 	for (int i = 0; i<n; i++)
 	{
-		for (int j = 0; j<m; j++)
-			cout << a[i][j] + b[i][j] << " ";
+		for (int j = 0; j<m; j++){
+			x[i][j] = a[i][j] + b[i][j];
+			cout <<  x[i][j] << " ";
+		}
 		cout << endl;
 	}
-
-
-	return 0;
 }
