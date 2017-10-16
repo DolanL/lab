9a3076a7b6c3a3072b2524f739ff84fc1c9a737c
DolanL <32385280+DolanL@users.noreply.github.com>
2017-10-16 19:03:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,7 @@
 #include <stdio.h>
 using namespace std;
 
-int main()
-{
+int main() {
 	int n, m;
 	cout << "razmer 1 yacheiki" << endl;
 	cin >> n;
@@ -13,23 +12,22 @@ int main()
 	int b[n][m];
 	int x[n][m];
 	cout << "vvod elementov 1 massiva" << endl;
-	for (int i = 0; i < n; i++){
-	for (int j = 0; j < m; j++){
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
 			cin >> a[i][j];
+		}
 	}
-	}
-		
+
 	cout << "vvod elementov 2 massiva" << endl;
-	for (int i = 0; i<n; i++){
-	for (int j = 0; j<m; j++){
+	for (int i = 0; i<n; i++) {
+		for (int j = 0; j<m; j++) {
 			cin >> b[i][j];
-	}
+		}
 	};
 	cout<<endl;
 
-	for (int i = 0; i<n; i++)
-	{
-		for (int j = 0; j<m; j++){
+	for (int i = 0; i<n; i++) {
+		for (int j = 0; j<m; j++) {
 			x[i][j] = a[i][j] + b[i][j];
 			cout <<  x[i][j] << " ";
 		}
